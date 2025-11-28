76ea32f8487190b9c3ed9edfd8d85bec9baccbb8
Oliver <oschi1303@gmail.com>
2025-11-29 00:39:30 +0100

    Update home page

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,5 +2,15 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+  return <HomeLayout
+      {...baseOptions()}
+      links={[
+          { text: 'Documentation', url: '/docs' },
+          { text: 'Download', url: 'https://modrinth.com/user/oliver' },
+          { text: 'GitHub', url: 'https://github.com/fancyinnovations' },
+          { text: 'Discord', url: 'https://discord.gg/ZUgYCEJUEx' },
+      ]}
+      githubUrl={"https://github.com/fancyinnovations"}
+
+  >{children}</HomeLayout>;
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,32 @@
-import Link from 'next/link';
+import {Card, Cards} from "fumadocs-ui/components/card";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
-      <p>
-        You can open{' '}
-        <Link href="/docs" className="font-medium underline">
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+        <h1 className="text-8xl font-bold mb-4 text-fd-primary">FancyInnovations</h1>
+        <p className="text-2xl mb-32">Innovative Plugins for Minecraft and Hytale</p>
+
+        <div className="w-1/2 max-w-1/2 mx-auto">
+            <Cards className="px-8">
+                <Card
+                    title="Minecraft Plugins"
+                    description="Explore our collection of innovative Minecraft plugins like FancyNpcs or FancyHolograms."
+                    href="/docs/minecraft-plugins"
+                />
+                <Card
+                    title="Hytale Plugins"
+                    description="Discover our first Hytale plugins, including FancyCore."
+                    href="/docs/hytale-plugins"
+                />
+            </Cards>
+
+            <Card
+                className="mx-8 mt-8"
+                title="FancyAnalytics"
+                description="Dive into FancyAnalytics, our powerful analytics tool."
+                href="/docs/fancyanalytics"
+            />
+        </div>
     </div>
   );
 }
